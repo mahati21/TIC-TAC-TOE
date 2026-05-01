92fee8e0b10237207d7d9b73c8da3f0e30bdfebc
mahati21 <mahatigr21@gmail.com>
2026-05-01 19:51:19 +0530

    Feature UC6 : To place the move on board

--- src/main/java/com/tictactoe/TicTacToe.java
+++ src/main/java/com/tictactoe/TicTacToe.java
@@ -4,67 +4,47 @@ import java.util.Scanner;
 
 public class TicTacToe {
 
-    public static void main(String[] args) {
-
-        char[][] board = {
-                {'-', '-', '-'},
-                {'-', '-', '-'},
-                {'-', '-', '-'}
-        };
-
-        for (int turn = 0; turn < 9; turn++) {
-
-            char currentPlayer = (turn % 2 == 0) ? 'X' : 'O';
-            System.out.println("\nPlayer " + currentPlayer + "'s turn");
-
-            int slot = getUserSlot();
-
-            int row = (slot - 1) / 3;
-            int col = (slot - 1) % 3;
-
-            if (validateMove(board, row, col)) {
-                board[row][col] = currentPlayer;
-            } else {
-                System.out.println("Invalid move! Try again.");
-                turn--;
-                continue;
-            }
-
-            printBoard(board);
-        }
-
-        System.out.println("Game Over!");
-    }
-
-    static int getUserSlot() {
-        Scanner sc = new Scanner(System.in);
-        int slot;
-
-        while (true) {
-            System.out.print("Enter slot (1-9): ");
-            slot = sc.nextInt();
-
-            if (slot >= 1 && slot <= 9) {
-                return slot;
-            } else {
-                System.out.println("Invalid input! Please enter between 1 and 9.");
-            }
-        }
-    }
-
-    static boolean validateMove(char[][] board, int row, int col) {
-        return (row >= 0 && row < 3 &&
-                col >= 0 && col < 3 &&
-                board[row][col] == '-');
-    }
-
-    static void printBoard(char[][] board) {
-        System.out.println();
+    static char[][] board = new char[3][3];
+    public static void initializeBoard() {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
+                board[i][j] = ' ';
             }
-            System.out.println();
         }
+    }
+
+    public static boolean placeMove(int row, int col, char symbol) {
+
+        if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+            System.out.println("Invalid position!");
+            return false;
+        }
+
+        if (board[row][col] != ' ') {
+            System.out.println("Cell already occupied!");
+            return false;
+        }
+
+        board[row][col] = symbol;
+        return true;
+    }
+
+    public static void printBoard() {
+        for (int i = 0; i < 3; i++) {
+            System.out.println(board[i][0] + " | " + board[i][1] + " | " + board[i][2]);
+            if (i < 2) {
+                System.out.println("--+---+--");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        initializeBoard();
+
+        placeMove(0, 0, 'X');
+        placeMove(1, 1, 'O');
+
+        printBoard();
     }
 }
